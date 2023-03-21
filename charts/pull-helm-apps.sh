a47b8f718455037204dae6e1431b81cee35e24f8
Nathan Grey <nathan.grey@megaport.com>
2023-03-21 15:33:46 -0400

    Updates nvidia-device-plugin helm chart name

--- charts/pull-helm-apps.sh
+++ charts/pull-helm-apps.sh
@@ -11,7 +11,7 @@ HELM_REPOS=( \
     'aws-ebs-csi-driver https://kubernetes-sigs.github.io/aws-ebs-csi-driver aws-ebs-csi-driver 2.17.2' \
     'k8s-dashboard https://kubernetes.github.io/dashboard kubernetes-dashboard 6.0.6' \
     'gpu-helm-charts https://nvidia.github.io/dcgm-exporter/helm-charts dcgm-exporter 3.1.3' \
-    'nvdp https://nvidia.github.io/k8s-device-plugin nvdp 0.13.0'
+    'nvdp https://nvidia.github.io/k8s-device-plugin nvidia-device-plugin 0.13.0' \
 )
 
 for repo in "${HELM_REPOS[@]}"
